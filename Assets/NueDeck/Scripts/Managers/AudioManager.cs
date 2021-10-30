ac50682adb1897b98f54805b81ac8eb10ded5765
Arefnue <arifmertcankara@gmail.com>
2021-10-30 13:27:20 +0300

    feat: Audio

--- Assets/NueDeck/Scripts/Managers/AudioManager.cs
+++ Assets/NueDeck/Scripts/Managers/AudioManager.cs
@@ -4,18 +4,17 @@ namespace NueDeck.Scripts.Managers
 {
     public class AudioManager : MonoBehaviour
     {
-        public static AudioManager instance;
+        public static AudioManager Instance;
 
         public AudioSource musicSource;
         public AudioSource sfxSource;
         public AudioSource buttonSource;
         
-        
         private void Awake()
         {
-            if (instance == null)
+            if (Instance == null)
             {
-                instance = this;
+                Instance = this;
 
                 DontDestroyOnLoad(gameObject);
             }
@@ -28,18 +27,28 @@ namespace NueDeck.Scripts.Managers
 
         public void PlayMusic(AudioClip clip)
         {
-            musicSource.clip = clip;
-            musicSource.Play();
+            if (clip)
+            {
+                musicSource.clip = clip;
+                musicSource.Play();
+            }
+           
         }
 
         public void PlayOneShot(AudioClip clip)
         {
-            sfxSource.PlayOneShot(clip);
+            if (clip)
+            {
+                sfxSource.PlayOneShot(clip);
+            }
         }
         
         public void PlayOneShotButton(AudioClip clip)
         {
-            buttonSource.PlayOneShot(clip);
+            if (clip)
+            {
+                buttonSource.PlayOneShot(clip);
+            }
         }
     }
 }
