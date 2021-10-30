eb296fb12e9d12a3455e2a83e3e96b5a2ff9ac0d
Arefnue <arifmertcankara@gmail.com>
2021-10-30 16:40:14 +0300

    cards added

---
+++ Assets/NueDeck/Scripts/Card/CardActions/ConvertPoisonToDamage.cs
@@ -0,0 +1,22 @@
+﻿using NueDeck.Scripts.Enums;
+using NueDeck.Scripts.Managers;
+using UnityEngine;
+
+namespace NueDeck.Scripts.Card.CardActions
+{
+    public class ConvertPoisonToDamage : CardActionBase
+    {
+        public override CardActionType ActionType => CardActionType.ConvertPoisonToDamage;
+        
+        public override void DoAction(CardActionParameters actionParameters)
+        {
+            if (actionParameters.targetCharacter)
+            {
+                actionParameters.targetCharacter.CharacterStats.Damage(actionParameters.targetCharacter.CharacterStats.statusDict[StatusType.Poison].StatusValue*Mathf.RoundToInt(actionParameters.value));
+                actionParameters.targetCharacter.CharacterStats.ClearStatus(StatusType.Poison);
+                FxManager.Instance.PlayFx(actionParameters.targetCharacter.transform,FxType.Heal);
+            }
+            AudioManager.Instance.PlayOneShot(actionParameters.cardData.audioType);
+        }
+    }
+}
---
+++ Assets/NueDeck/Scripts/Card/CardActions/ConvertPoisonToHeal.cs
@@ -0,0 +1,29 @@
+﻿using NueDeck.Scripts.Enums;
+using NueDeck.Scripts.Managers;
+using UnityEngine;
+
+namespace NueDeck.Scripts.Card.CardActions
+{
+    public class ConvertPoisonToHeal : CardActionBase
+    {
+        public override CardActionType ActionType => CardActionType.ConvertPoisonToHeal;
+        
+        public override void DoAction(CardActionParameters actionParameters)
+        {
+            if (actionParameters.targetCharacter)
+            {
+                actionParameters.targetCharacter.CharacterStats.Heal(actionParameters.targetCharacter.CharacterStats.statusDict[StatusType.Poison].StatusValue*Mathf.RoundToInt(actionParameters.value));
+                actionParameters.targetCharacter.CharacterStats.ClearStatus(StatusType.Poison);
+                FxManager.Instance.PlayFx(actionParameters.targetCharacter.transform,FxType.Heal);
+                
+            }
+            else
+            {
+                actionParameters.selfCharacter.CharacterStats.Heal(actionParameters.selfCharacter.CharacterStats.statusDict[StatusType.Poison].StatusValue*Mathf.RoundToInt(actionParameters.value));
+                actionParameters.selfCharacter.CharacterStats.ClearStatus(StatusType.Poison);
+                FxManager.Instance.PlayFx(actionParameters.selfCharacter.transform,FxType.Heal);
+            }
+            AudioManager.Instance.PlayOneShot(actionParameters.cardData.audioType);
+        }
+    }
+}
